2766c41b33c00d577fa80a32c5bf4b4b8415b28a
eggbrid <WX2890325>
2016-11-17 21:00:53 +0800

    image

--- app/src/main/java/com/shpro/xus/shproject/db/DBHelper.java
+++ app/src/main/java/com/shpro/xus/shproject/db/DBHelper.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
 import com.shpro.xus.shproject.bean.user.Account;
+import com.shpro.xus.shproject.bean.user.User;
+
+import java.sql.SQLException;
 
 /**
  * Created by xus on 2016/11/17.
@@ -13,10 +16,17 @@ import com.shpro.xus.shproject.bean.user.Account;
 public class DBHelper {
     public DBHelper(Context context) {
         dBConfig = OpenHelperManager.getHelper(context, DBConfig.class);
+        try {
+            user = dBConfig.getDao(User.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static DBHelper dBHelper;
     public static DBConfig dBConfig;
+    private static Dao<User, Integer> user;
 
     public static DBHelper getInstance(Context context) {
         if (dBHelper == null) {
@@ -25,5 +35,8 @@ public class DBHelper {
         return dBHelper;
     }
 
-    public Dao<Account, Integer> dao;
+    public void createUser(User accounts) throws SQLException {
+        user.create(accounts);
+    }
+
 }
