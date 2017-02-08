a7cde2e0991cbdc09e3c1d01f352941b6d5417c0
eggbrid <WX2890325>
2017-02-08 19:05:02 +0800

    寻找物品

---
+++ app/src/main/java/com/shpro/xus/shproject/bean/response/find/FindBagResponse.java
@@ -0,0 +1,10 @@
+package com.shpro.xus.shproject.bean.response.find;
+
+import com.shpro.xus.shproject.bean.response.TResponse;
+
+/**
+ * Created by xus on 2017/2/8.
+ */
+
+public class FindBagResponse extends TResponse<FindBagItem> {
+}
---
+++ app/src/main/java/com/shpro/xus/shproject/view/views/BagShowDialog.java
@@ -0,0 +1,89 @@
+package com.shpro.xus.shproject.view.views;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.shpro.xus.shproject.R;
+import com.shpro.xus.shproject.bean.Bag;
+import com.shpro.xus.shproject.util.BagUtil;
+import com.shpro.xus.shproject.util.CommentUtil;
+import com.shpro.xus.shproject.util.ImageLoaderUtil;
+import com.shpro.xus.shproject.view.BaseActivity;
+
+/**
+ * Created by xus on 2016/11/18.
+ */
+
+public class BagShowDialog extends Dialog implements View.OnClickListener {
+    protected ImageView icon;
+    protected TextView info;
+    protected Button use;
+    protected Button d;
+    protected Bag bag;
+    protected BaseActivity context;
+
+    public BagShowDialog(BaseActivity context, Bag bag) {
+        super(context, R.style.Dialog_FullscreenB);
+        this.bag = bag;
+        this.context = context;
+    }
+
+    public BagShowDialog(BaseActivity context, int themeResId) {
+        super(context, R.style.Dialog_FullscreenB);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_show_bag);
+        initView();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.use) {
+            BagUtil.getInstance().addBag(context, bag, true, 1, new BagUtil.OnSuccess() {
+                @Override
+                public void onSuccess(int flg) {
+                    BagShowDialog.this.dismiss();
+                }
+            });
+        } else if (view.getId() == R.id.d) {
+            this.dismiss();
+        }
+    }
+
+    private void initView() {
+        icon = (ImageView) findViewById(R.id.icon);
+        info = (TextView) findViewById(R.id.info);
+        use = (Button) findViewById(R.id.use);
+        use.setOnClickListener(BagShowDialog.this);
+        d = (Button) findViewById(R.id.d);
+        d.setOnClickListener(BagShowDialog.this);
+        info.setText(bag.getInfo());
+        if (bag.getAction().equals("1") || bag.getAction().equals("3")) {
+            use.setVisibility(View.VISIBLE);
+        } else {
+            use.setVisibility(View.GONE);
+        }
+        if (TextUtils.isEmpty(bag.getIcon())) {
+            icon.setImageResource(R.drawable.shpg_unno);
+        } else if (bag.getIcon().startsWith("http:")) {
+            ImageLoaderUtil.getInstance().loadNomalImage(bag.getIcon(), icon);
+        } else if (bag.getIcon().startsWith("shpg")) {
+            icon.setImageResource(CommentUtil.getIcon(bag.getIcon(), context));
+        } else {
+            icon.setImageResource(R.drawable.shpg_unno);
+        }
+
+    }
+
+
+}
