20ea1ee1259bd009fd864f950bf4da9e1d5e1c9e
eggbrid <1340888417@qq.com>
2017-04-11 12:01:06 +0800

    答题框架确定

---
+++ app/src/main/java/com/shpro/xus/shproject/view/taobao/TBOneDayActivity.java
@@ -0,0 +1,21 @@
+package com.shpro.xus.shproject.view.taobao;
+
+import android.app.Activity;
+
+import com.shpro.xus.shproject.view.BaseActivity;
+
+/**
+ * Created by xus on 2017/4/11.
+ */
+
+public class TBOneDayActivity extends BaseActivity {
+    @Override
+    public int setContentView() {
+        return 0;
+    }
+
+    @Override
+    public void initView() throws Exception {
+
+    }
+}
